8e3b386b83e764d93c7a8a84f8a87736fcbfc7aa
imogenkraak <imogen.kraak@gmail.com>
2020-11-03 18:20:38 +0000

    keys pressed logged on console

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Person from "../person";
+import KeyPressed from "../../hooks/keys";
 
 export default function Player({ skin }) {
     const data = {
         h: 32,
         w: 32
       };
+
+      KeyPressed((e) => {
+        const dir = e.key.replace("Arrow", "").toLowerCase()
+        console.log(dir)
+      })
+
     return <Person sprite={`/sprites/${skin}.png`} data={data} />
 }
